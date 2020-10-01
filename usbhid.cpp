c3ba9cf7347b8d815471e9d21f6b4427ba007990
Dirk Jagdmann <doj@cubic.org>
2020-10-01 16:37:00 -0700

    remove commented code

--- usbhid.cpp
+++ usbhid.cpp
@@ -17,20 +17,7 @@ e-mail   :  support@circuitsathome.com
 
 #include "usbhid.h"
 
-//get HID report descriptor
-
-/* WRONG! Endpoint is _ALWAYS_ ZERO for HID! We want the _INTERFACE_ value here!
-uint8_t USBHID::GetReportDescr(uint8_t ep, USBReadParser *parser) {
-        const uint8_t constBufLen = 64;
-        uint8_t buf[constBufLen];
-
-        uint8_t rcode = pUsb->ctrlReq(bAddress, ep, bmREQ_HID_REPORT, USB_REQUEST_GET_DESCRIPTOR, 0x00,
-                HID_DESCRIPTOR_REPORT, 0x0000, 128, constBufLen, buf, (USBReadParser*)parser);
-
-        //return ((rcode != hrSTALL) ? rcode : 0);
-        return rcode;
-}
- */
+/// get HID report descriptor
 uint8_t USBHID::GetReportDescr(uint16_t wIndex, USBReadParser *parser) {
         const uint8_t constBufLen = 64;
         uint8_t buf[constBufLen];
